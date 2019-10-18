3651d1f66e306c5e5e62ec30e3d2dc4d516f5a88
baripembo <erikawei@gmail.com>
2019-10-18 18:48:11 -0400

    init

---
+++ src/js/helpers.js
@@ -0,0 +1,32 @@
+function hxlProxyToJSON(input){
+    var output = [];
+    var keys=[]
+    input.forEach(function(e,i){
+        if(i==0){
+            e.forEach(function(e2,i2){
+                var parts = e2.split('+');
+                var key = parts[0]
+                if(parts.length>1){
+                    var atts = parts.splice(1,parts.length);
+                    atts.sort();                    
+                    atts.forEach(function(att){
+                        key +='+'+att
+                    });
+                }
+                keys.push(key);
+            });
+        } else {
+            var row = {};
+            e.forEach(function(e2,i2){
+                row[keys[i2]] = e2;
+            });
+            output.push(row);
+        }
+    });
+    return output;
+}
+
+function startOfWeek(date) {
+    var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -7 : 0);
+    return new Date(date.setDate(diff)); 
+}
